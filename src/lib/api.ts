6672d408e717a89aa44a4fde8c4cb0a5d043c6a8
Juanpapb0401 <e5jparra@gmail.com>
2025-11-13 16:31:31 -0500

    fix: something

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.BASE_URL || "http://localhost:9000/api/",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
@@ -10,10 +10,10 @@ const instance = axios.create({
 instance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('authToken');
-        if(token) config.headers.Authorization = `Bearer ${token}`
+        if (token) config.headers.Authorization = `Bearer ${token}`
         return config;
     },
-    (error) =>{
+    (error) => {
         return Promise.reject(error);
     }
 )
